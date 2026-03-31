5ee85de5b4a503813a1662d66237745754a5d009
PooLX23 <146299573+PooLX23@users.noreply.github.com>
2026-03-31 12:58:39 +0200

    Add BRAK option for initial fee field in vehicle rows

---
+++ frontend/src/services/api.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+import { env } from '../config/env'
+
+export async function submitApplication({ token, type, data, files }) {
+  const formData = new FormData()
+  formData.append('payload', JSON.stringify(data))
+  files.forEach((file) => formData.append('files', file))
+
+  const url = `${env.apiBaseUrl}/applications/${type}`
+  const response = await axios.post(url, formData, {
+    headers: { Authorization: `Bearer ${token}` }
+  })
+  return response.data
+}
+
+export async function fetchCarGroups(token) {
+  const url = `${env.apiBaseUrl}/applications/car-groups`
+  const response = await axios.get(url, {
+    headers: { Authorization: `Bearer ${token}` }
+  })
+  return response.data?.items || []
+}
